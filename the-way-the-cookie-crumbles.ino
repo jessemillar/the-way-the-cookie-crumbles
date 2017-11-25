02174b9ff00b7011ca120bb026f50a8734b594de
Jesse Millar <hellojessemillar@gmail.com>
2017-11-25 10:31:22 -0600

    Simplifying boot

--- the-way-the-cookie-crumbles.ino
+++ the-way-the-cookie-crumbles.ino
@@ -7,7 +7,23 @@ Player player;
 void setup()
 {
 	arduboy.setFrameRate(30);
-	arduboy.begin();
+
+	arduboy.boot(); // raw hardware
+	arduboy.blank(); // blank the display
+
+	arduboy.flashlight(); // light the RGB LED and screen if UP button is being held.
+
+	// check for and handle buttons held during start up for system control
+	arduboy.systemButtons();
+
+	arduboy.audio.begin();
+
+	// wait for all buttons to be released
+	do
+	{
+		arduboy.delayShort(50);
+	}
+	while (arduboy.buttonsState());
 }
 
 void loop()
